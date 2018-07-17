8193e6165ce3ecaacaab45622372b28f240dd590
hiveViewhy <hyw@hiveviewhydemacbook-pro.local>
2018-07-17 15:52:13 +0800

    Initial Commit

---
+++ DownloadCenter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DownloadCenter
+//
+//  Created by hyw on 2018/7/17.
+//  Copyright © 2018年 haiyang_wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
